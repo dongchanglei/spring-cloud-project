cfe6d760a6c2e37ab30bc4f3c1359138713b6ee7
dongzhanglei <dongzhanglei@taoche.com>
2021-04-21 08:54:41 +0800

    gateway commit

---
+++ gateway/src/main/java/com/GateWayApp.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关
+ *
+ * @author dcl
+ * @since 2021/4/20
+ **/
+@SpringBootApplication
+public class GateWayApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GateWayApp.class);
+    }
+
+}
